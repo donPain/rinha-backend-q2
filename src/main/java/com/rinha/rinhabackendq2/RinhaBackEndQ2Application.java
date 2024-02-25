6f1b1b07318e32c8768c3b5b06103a285f0b9bb9
eduardo <eduardo.plis@hotmail.com>
2024-02-24 21:10:14 -0300

    init

---
+++ src/main/java/com/rinha/rinhabackendq2/RinhaBackEndQ2Application.java
@@ -0,0 +1,13 @@
+package com.rinha.rinhabackendq2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackEndQ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackEndQ2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinha/rinhabackendq2/RinhaBackEndQ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinha.rinhabackendq2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackEndQ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
